05796cdd61b85e19f00a2341178757312e2212ac
lucas palacios <lucaspalacios90@gmail.com>
2021-10-03 23:55:07 -0300

    backend finalizado

--- Backend/src/routes/Videos.controller.ts
+++ Backend/src/routes/Videos.controller.ts
@@ -1,22 +1,46 @@
 import {RequestHandler} from 'express'
+import Video from "./Videos";
 
-export const getVideo : RequestHandler = (req, res) => {
-    res.json('Getting a Video...')
+export const getVideo : RequestHandler = async (req, res) => {
+    const videoFound = await Video.findById(req.params.id);
+
+    if (!videoFound) {
+        return res.status(204).json();
+    }
+    res.json(videoFound);
 };
 
-export const getAllVideos : RequestHandler = (req, res) => {
-    res.json('Getting all Videos...')
+export const getAllVideos : RequestHandler = async (req, res) => {
+    try {
+        const videos = await Video.find();
+    res.json(videos);
+    } catch (error) {
+        res.json(error);
+    }
 };
 
-export const createVideo : RequestHandler = (req, res) => {
-    res.json('Video was created...')
+export const createVideo : RequestHandler = async (req, res) => {
+    const videoFound = await Video.findOne({url: req.body.url});
+    if (videoFound) {
+        return res.status(301).json({message: 'The Url already exists'});
+    }
+    const video = new Video(req.body);
+    const savedVideo = await video.save();
+    res.json(savedVideo);
 };
 
-export const deleteVideo : RequestHandler = (req, res) => {
-    res.json('Video was deleted...')
+export const deleteVideo : RequestHandler = async (req, res) => {
+    const videoFound = await Video.findByIdAndDelete(req.params.id);
+
+    if (!videoFound) {
+        return res.status(204).json();
+    }
+    res.json(videoFound);
 };
 
-export const updateVideo : RequestHandler = (req, res) => {
-    res.json('Video was updated...')
+export const updateVideo : RequestHandler = async (req, res) => {
+    const videoUpdate = await Video.findByIdAndUpdate(req.params.id, req.body, {new: true});
+    if (!videoUpdate) return res.status(204).json();
+    res.json(videoUpdate);
 };
 
